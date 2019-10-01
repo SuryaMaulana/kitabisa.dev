eba37dd55741382e7c9e2cbc814f6b5628aeed2e
Muhammad Surya Maulana <battletank19@gmail.com>
2019-10-01 20:27:05 +0700

    a

--- resources/views/pages/zakat.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <h1 class="font-weight-bold text-center">Under Development. 
-    <br>we make sure to notice you when its done</h1>
-</div>
-@endsection
-
